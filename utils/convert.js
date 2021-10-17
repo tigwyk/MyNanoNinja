0dc5e42ce8eaa27e9abbd0673182f4f1c605b94f
Lee Ingram <lee@soylentcola.com>
2021-10-17 15:59:28 -0700

    Banano adjustments

--- utils/convert.js
+++ utils/convert.js
@@ -2,12 +2,12 @@ const BigNumber = require('big.js');
 
 function fromRaw(raw){
     const value = BigNumber(raw.toString());
-    return value.shiftedBy((config.currency.precision || 30) * -1).toNumber();
+    return value.shiftedBy((config.currency.precision || 29) * -1).toNumber();
 }
 exports.fromRaw = fromRaw;
 
 function toRaw(mvalue){
     const value = BigNumber(mvalue.toString());
-    return value.shiftedBy(30).toString();
+    return value.shiftedBy(29).toString();
 }
 exports.toRaw = toRaw;
--- utils/matomo.js
+++ utils/matomo.js
@@ -16,7 +16,6 @@ exports = module.exports = function analytics(options) {
               '1': ['API version', 'v1'],
               '2': ['HTTP method', req.method]
             }),
-            token_auth: options.matomoToken,
             cip: req.ip
 
         });
